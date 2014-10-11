e4b734e20f80e1ef8bcc1f1e44c424435b0ed74c
main() <main@ehvag.eu.org>
2014-10-11 17:25:21 +0200

    WebAPI now actually does stuff

--- WebAPI/GenerateHeatmap.ashx.cs
+++ WebAPI/GenerateHeatmap.ashx.cs
@@ -2,9 +2,12 @@
 namespace WebAPI
 {
 	using System;
+	using System.IO;
+	using System.Linq;
 	using System.Web;
 	using System.Web.UI;
-	using Newtonsoft.Json;
+	using Newtonsoft.Json.Linq;
+	using HeatmapGenerator;
 
 	public class GenerateHeatmap : System.Web.IHttpHandler
 	{
@@ -29,16 +32,17 @@ namespace WebAPI
 			    && Single.TryParse(req.Form["posY"], out posY)
 			    && Single.TryParse(req.Form["scale"], out scale)) {
 
-				// TODO: invoke demo parser [API pls]
+				var requestId = Guid.NewGuid().ToString();
+				var basepath = Path.Combine("/usr/share/nginx/www-demo/static/results", requestId);
+				Directory.CreateDirectory(basepath);
+				var files = new Heatmap(req.Files["demo"].InputStream, posX, posY, scale).Parse();
+				foreach (var item in files) {
+					item.Value.Save(Path.Combine(basepath, item.Key + ".png"), System.Drawing.Imaging.ImageFormat.Png);
+				}
 
 				context.Response.ContentType = "application/json";
-				context.Response.Output.Write(JsonConvert.SerializeObject(new {
-					FlashMap = "result/flashmap.png",
-					DecoyMap = "result/decoymap.png",
-					SmokeMap = "result/smokemap.png",
-					HEMap = "result/hemap.png",
-					MolotovMap = "result/molotovmap.png",
-				}));
+				var webBase = "/results/" + requestId + "/";
+				context.Response.Output.WriteLine(new JObject(files.Select(pair => new JProperty(pair.Key, webBase + pair.Key + ".png"))));
 			} else
 				throw new Exception("invalid form data (posx, posy or scale)");
 		}
