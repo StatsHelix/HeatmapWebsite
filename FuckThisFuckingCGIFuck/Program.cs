31861e0e8fb688ab752fdd15270e366238af4fd6
moritzuehling <github@moritzuehling.de>
2014-10-12 10:19:17 +0200

    Made more stable, implemented multithreading.

--- FuckThisFuckingCGIFuck/Program.cs
+++ FuckThisFuckingCGIFuck/Program.cs
@@ -6,6 +6,8 @@ using System.Threading;
 using System.Collections.Concurrent;
 using HeatmapGenerator;
 using Newtonsoft.Json;
+using System.Threading;
+using System.Globalization;
 
 namespace FuckThisFuckingCGIFuck
 {
@@ -18,6 +20,22 @@ namespace FuckThisFuckingCGIFuck
 			var listener = new HttpListener();
 			listener.Prefixes.Add("http://localhost:5500/");
 			listener.Start();
+
+			for (int i = 0; i < int.Parse(args[0]); i++) {
+				Thread t = new Thread(new ParameterizedThreadStart(Work));
+				t.IsBackground = true;
+				t.CurrentCulture = CultureInfo.InvariantCulture;
+				t.CurrentUICulture = CultureInfo.InvariantCulture;
+				t.Name = "Webserver-Listen-Slave #"+(i+1);
+				t.Start((object)listener);
+			}
+
+			Thread.Sleep(int.MaxValue);
+		}
+
+		public static void Work(object data)
+		{
+			var listener (HttpListener)data;
 			while (true) {
 				ProcessRequest(listener.GetContext());
 			}
@@ -26,19 +44,28 @@ namespace FuckThisFuckingCGIFuck
 		private static string MULTIPART_PREFIX = "multipart/form-data; boundary=";
 		public static void ProcessRequest (HttpListenerContext context)
 		{
+			try
+			{
+				HandleRequest(context);
+			}
+			catch(Exception e)
+			{
+
+			}
+		}
+
+		static void HandleRequest(HttpListenerContext context)
+		{
 			var req = context.Request;
-		
-
 			var multipart = new HttpMultipart(req.InputStream, req.ContentType.Substring(MULTIPART_PREFIX.Length), req.ContentEncoding, "demo");
-
 			multipart.ReadBoundary();
 			var eMapImage = multipart.ReadNextElement(5 * 1024 * 1024);
 			var ePosX = multipart.ReadNextElement(256);
 			var ePosY = multipart.ReadNextElement(256);
 			var eScale = multipart.ReadNextElement(256);
 			var eWatermark = multipart.ReadNextElement(256);
-			var eDemo = multipart.ReadNextElement(256); // shall be null
-
+			var eDemo = multipart.ReadNextElement(256);
+			// shall be null
 			if (eMapImage.Name != "map")
 				throw new Exception("emapimg invalid");
 			if (ePosX.Name != "posX")
@@ -51,44 +78,32 @@ namespace FuckThisFuckingCGIFuck
 				throw new Exception("ewatermark invalid");
 			if (eDemo != null)
 				throw new Exception("lolwat");
-
 			context.Response.ContentType = "application/json";
-
 			var writer = new StreamWriter(context.Response.OutputStream);
 			writer.AutoFlush = true;
-			/*
-			var writer = new StreamWriter(context.Response.OutputStream);
-			writer.WriteLine(
-				JsonConvert.SerializeObject(new { result = String.Format(
-					"eMapImage={0} ePosX={1} ePosY={2} eScale={3} eWatermark={4} eDemo={5}",
-					eMapImage, ePosX, ePosY, eScale, eWatermark, eDemo) }));
-			writer.Flush();
-			context.Response.OutputStream.Flush();
-			context.Response.Close();
-			context.Request.InputStream.Close();*/
-
 			Image map;
 			try {
 				using (var stream = new MemoryStream(eMapImage.Data))
 					map = Image.FromStream(stream);
 				if (map == null)
 					throw new ArgumentNullException("map");
-				if ((map.Width != 1024) || (map.Height != 1024)) {
-					writer.WriteLine(JsonConvert.SerializeObject(new { result = "mapsize" }));
+				if (( map.Width != 1024 ) || ( map.Height != 1024 )) {
+					writer.WriteLine(JsonConvert.SerializeObject(new {
+						result = "mapsize"
+					}));
 					context.Response.Close();
 					return;
 				}
-			} catch (Exception) {
-				writer.WriteLine(JsonConvert.SerializeObject(new { result = "mapformat" }));
+			}
+			catch (Exception) {
+				writer.WriteLine(JsonConvert.SerializeObject(new {
+					result = "mapformat"
+				}));
 				context.Response.Close();
 				return;
 			}
-
 			float posX, posY, scale;
-			if (Single.TryParse(ePosX.Text, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out posX)
-				&& Single.TryParse(ePosY.Text, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out posY)
-				&& Single.TryParse(eScale.Text, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out scale)) {
-
+			if (Single.TryParse(ePosX.Text, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out posX) && Single.TryParse(ePosY.Text, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out posY) && Single.TryParse(eScale.Text, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out scale)) {
 				var requestId = Guid.NewGuid().ToString();
 				var basepath = Path.Combine("/usr/share/nginx/www-demo/static/results", requestId);
 				Directory.CreateDirectory(basepath);
@@ -97,10 +112,13 @@ namespace FuckThisFuckingCGIFuck
 					item.Value.Save(Path.Combine(basepath, item.Key + ".png"), System.Drawing.Imaging.ImageFormat.Png);
 				}
 				map.Save(Path.Combine(basepath, "map.png"), System.Drawing.Imaging.ImageFormat.Png);
-
-				writer.WriteLine(JsonConvert.SerializeObject(new { result = "success", id = requestId }));
+				writer.WriteLine(JsonConvert.SerializeObject(new {
+					result = "success",
+					id = requestId
+				}));
 				context.Response.Close();
-			} else
+			}
+			else
 				throw new Exception("invalid form data (posx, posy or scale) ");
 		}
 	}
